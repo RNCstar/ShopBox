7b7b9019329a6ff3acf171b13b6e4be744287623
RNCstar <r.n.c.star2002@gmail.com>
2024-06-04 12:35:49 -0700

    add make factor

--- js/app.js
+++ js/app.js
@@ -1,38 +1,39 @@
 const products = [
   {
     id: 1,
-    name: "Iphone12",
+    name: "iPhone 12",
     price: 2000,
-    img: "../img/1.png",
+    image: "../img/1.png",
   },
   {
     id: 2,
-    name: "Iphone13",
+    name: "iPhone 13",
     price: 3500,
-    img: "../img/2.webp",
+    image: "../img/2.webp",
   },
   {
     id: 3,
     name: "Samsung",
-    price: 2700,
-    img: "../img/3.jpg",
+    price: 2600,
+    image: "../img/3.jpg",
   },
 ];
-const renderProduct = () => {
-  const productsDiv = document.querySelector(".products");
-  productsDiv.innerHTML = "";
+
+const renderProducts = () => {
+  const productDiv = document.querySelector(".products");
+  productDiv.innerHTML = "";
 
   products.forEach((item, index) => {
-    productsDiv.innerHTML += `<div class="product card">
-              <div class="producImg">
-                <img src=${item.img} class="img-fluid" alt="" />
-              </div>
-              <h2 class="productTitle">${item.name}</h2>
-              <h2 class="productPrice">${item.price + "$"}</h2>
-              <button type="button" class="btn btn-primary">
-                افزودن به سبد خرید
-              </button>
-            </div>`;
+    productDiv.innerHTML += `
+        <div class="product card">
+            <div class="producImg">
+                <img src=${item.image}>
+            </div>
+            <h2 class="productTitle">${item.name}</h2>
+            <h3 class="productPrice">${item.price} $</h3>
+            <button class="btn btn-primary" onclick="addToCart(${index})">افزودن به سبد خرید</button>
+        </div>
+        `;
   });
 };
 
@@ -41,45 +42,73 @@ let cart = {
   total: 0,
 };
 
-const renderItems = () => {
+const renderCartItems = () => {
   const cartDiv = document.querySelector(".cartItems");
   cartDiv.innerHTML = "";
-  const totalPriceEL = document.querySelector(".cartTotalPrice");
+
+  const totalPriceEl = document.querySelector(".cartTotalPrice");
+
   let totalPrice = 0;
+
   if (cart.items.length === 0) {
-    cartDiv.innerHTML = "هیجی هنوز نخریدی";
+    cartDiv.innerHTML = "محصولی در سبد خرید وجود ندارد!";
   }
+
   cart.items.forEach((item) => {
     totalPrice += item.total;
-    cartDiv.innerHTML += `<div class="cartItem">
-                <div class="col-md-4">
-                  <button type="button" class="btn btn-danger">حذف</button>
-                </div>
-                <div class="col-md-4 p-8">
-                  <div class="qty">${item.qty}</div>
-                </div>
-                <div class="col-md-4">
-                  <h3 class="cartItemTitle">${item.name}</h3>
-                </div>
-              </div>`;
+    cartDiv.innerHTML += `
+        <div class="cartItem">
+            <div class="col-md-4">
+                <button class="btn btn-danger">حذف</button>
+            </div>
+            <div class="col-md-4 p-8">
+                <div class="qty">${item.qty}</div>
+            </div>
+            <div class="col-md-4">
+                <h3 class="cartItemTitle">${item.name}</h3>
+            </div>
+        </div>
+        `;
   });
-  totalPriceEL.innerHTML = `مجموع کل : ${totalPrice} $`;
+
+  totalPriceEl.innerHTML = `مجموع: ${totalPrice} $`;
 };
 
 const addToCart = (productIndex) => {
   const product = products[productIndex];
+
   let existingProduct = false;
-  const newCartItem = cart.items.reduce((state, item) => {
+
+  let newCartItems = cart.items.reduce((state, item) => {
     if (item.name === product.name) {
       existingProduct = true;
+
       const newItem = {
         ...item,
         qty: item.qty + 1,
         total: (item.qty + 1) * item.price,
       };
+
+      return [...state, newItem];
     }
+
+    return [...state, item];
   }, []);
+
+  if (!existingProduct) {
+    newCartItems.push({
+      ...product,
+      qty: 1,
+      total: product.price,
+    });
+  }
+
+  cart = {
+    ...cart,
+    items: newCartItems,
+  };
+  renderCartItems();
 };
 
-renderProduct();
-renderItems();
+renderProducts();
+renderCartItems();
